609534012aa642984bb0e7bfd5c8a0e39dec87f5
Alvaro Atienza <alv.atienza@gmail.com>
2015-11-26 11:00:15 -0500

    cleanup!

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var monk = require('monk');
 var db = monk('localhost:27017/test');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -38,7 +37,6 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
